72245711a684bc0f465ce64ce3efc9ead7de6e70
rekik maylef <rekikm@creavers.com>
2022-08-26 11:40:27 +0000

    Feature/admission rm

---
+++ HospitalManagementSystem.API/Configuration/AdmissionConfigurations.cs
@@ -0,0 +1,43 @@
+﻿using HospitalManagementSystem.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HospitalManagementSystem.API.Configuration
+{
+    public class AdmissionConfigurations : IEntityTypeConfiguration<Admission>
+    {
+        public void Configure(EntityTypeBuilder<Admission> builder)
+        {
+            builder.Property(a => a.Id)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(a => a.AdmissionTypeId)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(a => a.PatientId)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(a => a.EmployeeId)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(a => a.RoomId)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(a => a.WardId)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(a => a.AdmissionTime)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(a => a.AdmissionDate)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(a => a.DischargeDate)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(a => a.IsDischarge)
+                .IsRequired()
+                .HasMaxLength(50);
+         }
+        }
+}

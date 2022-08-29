a9df7379432d196e983a20530fdaeb62a661d985
selamawitlb <selamawitlb@gmail.com>
2022-08-29 14:35:20 +0300

    MedSupplier

---
+++ HospitalManagementSystem.API/Configuration/MedSupplierConfigurations.cs
@@ -0,0 +1,23 @@
+﻿using HospitalManagementSystem.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HospitalManagementSystem.API.Configuration
+{
+    public class MedSupplierConfigurations : IEntityTypeConfiguration<MedSupplier>
+    {
+        public void Configure(EntityTypeBuilder<MedSupplier> builder)
+        {
+            builder.Property(ms => ms.Name)
+                 .IsRequired()
+                 .HasMaxLength(60);
+            builder.Property(ms => ms.Description)
+               
+                .HasMaxLength(225);
+
+            builder.Property(ms => ms.PhoneNumber)
+                .IsRequired();
+                
+        }
+    }
+}
---
+++ HospitalManagementSystem.API/Models/MedSupplier.cs
@@ -0,0 +1,12 @@
+﻿using HospitalManagementSystem.API.Models.Base;
+
+namespace HospitalManagementSystem.API.Models
+{
+    public class MedSupplier:BaseModel
+    { 
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+    
+
+}
+}
